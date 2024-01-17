93968456fdfb7ed8ef8aee9149437e56f23d1d3d
andrei-novikav <80758126+andrei-novikav@users.noreply.github.com>
2024-01-17 19:42:27 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const { Client, MessageActionRow, MessageButton, MessageEmbed, Intents, TextInputComponent, Modal} = require('discord.js');
+const axios = require('axios');
+const express = require('express');
 const pg = require('pg');
 require('dotenv').config();
+const client = new Client({intents:
+        [Intents.FLAGS.GUILDS,
+            Intents.FLAGS.MESSAGE_CONTENT,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_TYPING,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.DIRECT_MESSAGE_TYPING], partials: ['CHANNEL', 'MESSAGE']});
 
+const app = express();
+
+const botToken = process.env["devBotToken"];
 const databaseUrl = process.env.databaseUrl;
 
 const pgClient = new pg.Client(databaseUrl);
@@ -11,17 +24,21 @@ pgClient.connect(err => {
     }
 });
 
-let streams = [];
+let streams;
 
-const loadStreamsFromDb = () => {
-    pgClient.query('Select * from streams', (err, result) => {
-        if (err) {
-            return console.error("Unable to fetch streams from DB", err);
-        }
-    
-        streams = result.rows;
-        console.info("Fetched  : " + result.rowCount + " streams from db");
-    });
+const loadStreamsFromDb = async () => {
+    try {
+        const result = await pgClient.query('Select * from streams');
+        streams = result.rows.map(el => ({
+            ...el,
+            channelId: el.channel_id,
+            messageText: el.message,
+            intervalInMinutes: parseInt(el.interval),
+        }))
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
 
 const saveStreamToDb = async (stream) => {
@@ -29,11 +46,12 @@ const saveStreamToDb = async (stream) => {
         await pgClient.query(
             `INSERT INTO public.streams(
                 name, token, channel_id, message, "interval")
-                VALUES (${stream.name}, ${stream.token}, ${stream.channelId}, ${stream.messageText}, ${stream.intervalInMinutes})`
+                VALUES ('${stream.name}', '${stream.token}', '${stream.channelId}', '${stream.messageText}', '${stream.intervalInMinutes}')`
         );
         console.log("Stream was saved to DB: " + JSON.stringify(stream));
     } catch (err) {
-        console.err("Error while saving stream to DB " + JSON.stringify(stream));
+        console.error("Error while saving stream to DB " + JSON.stringify(stream));
+        throw new Error(err);
     } finally {
         pgClient.end();
     }
@@ -48,7 +66,7 @@ const updateStreamInDb = async (stream) => {
         );
         console.log("Stream was updated in DB: " + JSON.stringify(stream));
     } catch (err) {
-        console.err("Error while updating stream in DB " + JSON.stringify(stream));
+        console.error("Error while updating stream in DB " + JSON.stringify(stream));
     } finally {
         pgClient.end();
     }
@@ -59,73 +77,137 @@ const deleteStream = async (stream) => {
         await pgClient.query(
             `Delete from streams WHERE streams.id=${stream.id};`
         );
-        str
         console.log("Stream was updated in DB: " + JSON.stringify(stream));
     } catch (err) {
-        console.err("Error while updating stream in DB " + JSON.stringify(stream));
+        console.error("Error while updating stream in DB " + JSON.stringify(stream));
     } finally {
         pgClient.end();
     }
 }
 
-const { Client, MessageActionRow, MessageButton, MessageEmbed, Intents, TextInputComponent, Modal} = require('discord.js');
-const axios = require('axios');
-const client = new Client({intents:
-        [Intents.FLAGS.GUILDS,
-            Intents.FLAGS.MESSAGE_CONTENT,
-            Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.GUILD_MESSAGE_TYPING,
-            Intents.FLAGS.DIRECT_MESSAGES,
-            Intents.FLAGS.DIRECT_MESSAGE_TYPING], partials: ['CHANNEL', 'MESSAGE']});
-
-const express = require('express');
-
-const app = express();
-
-const botToken = process.env.botToken;
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const onViewStreamsClick = async (interaction) => {
+    let streamsText = streams.map((stream, index) => `Поток ${index + 1}: ${stream.name}`).join('\n');
+    await interaction.reply({ content: streamsText || 'Нет активных потоков' });
+}
+
+const onDeleteStreamClick = async (interaction) => {
+    const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Введите номер потока')
+        .setDescription('Введите номер потока, который вы хотите удалить, в следующем формате:\n/deletestream Номер потока');
+    await interaction.reply({embeds: [embed]});
+}
+
+const onViewStreamInfoClick = async (interaction) => {
+    const description = streams.map((el, index) => `${index + 1}: Название - ${el.name}, Токен - ${el.token}, Айди канала - ${el.channelId}\n`)
+    const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Все потоки')
+        .setDescription(description.join(""));
+    await interaction.reply({embeds: [embed]});
+}
+
+const onCreationModalClick = async (interaction) => {
+    const modal = new Modal()
+        .setCustomId('myModal')
+        .setTitle('My Modal');
+    const nameInput =  new TextInputComponent()
+        .setCustomId('nameInput')
+        .setLabel("Название потока")
+        .setStyle('SHORT');
+    const tokenInput = new TextInputComponent()
+        .setCustomId('tokenInput')
+        .setLabel("Твой токен")
+        .setStyle('SHORT');
+    const channelIdInput = new TextInputComponent()
+        .setCustomId('channelIdInput')
+        .setLabel("ID канала")
+        .setStyle('SHORT');
+    const messageTextInput = new TextInputComponent()
+        .setCustomId('messageTextInput')
+        .setLabel("Текст сообщения")
+        .setStyle('PARAGRAPH');
+    const periodInput = new TextInputComponent()
+        .setCustomId('periodInput')
+        .setLabel("Период в минутах")
+        .setStyle('SHORT');
+    const nameActionRow = new MessageActionRow().addComponents(nameInput);
+    const firstActionRow = new MessageActionRow().addComponents(tokenInput);
+    const secondActionRow = new MessageActionRow().addComponents(channelIdInput);
+    const thirdActionRow = new MessageActionRow().addComponents(messageTextInput);
+    const lastActionRow = new MessageActionRow().addComponents(periodInput);
+    modal.addComponents(nameActionRow, firstActionRow, secondActionRow, thirdActionRow, lastActionRow);
+    await interaction.showModal(modal);
+}
+
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isButton()) return;
-    if (interaction.customId  === 'show') {
-        const modal = new Modal()
-            .setCustomId('myModal')
-            .setTitle('My Modal');
-        const nameInput =  new TextInputComponent()
-            .setCustomId('nameInput')
-            .setLabel("Название потока")
-            .setStyle('SHORT');
-        const tokenInput = new TextInputComponent()
-            .setCustomId('tokenInput')
-            .setLabel("Твой токен")
-            .setStyle('SHORT');
-        const channelIdInput = new TextInputComponent()
-            .setCustomId('channelIdInput')
-            .setLabel("ID канала")
-            .setStyle('SHORT');
-        const messageTextInput = new TextInputComponent()
-            .setCustomId('messageTextInput')
-            .setLabel("Текст сообщения")
-            .setStyle('PARAGRAPH');
-        const periodInput = new TextInputComponent()
-            .setCustomId('periodInput')
-            .setLabel("Период в минутах")
-            .setStyle('SHORT');
-        const nameActionRow = new MessageActionRow().addComponents(nameInput);
-        const firstActionRow = new MessageActionRow().addComponents(tokenInput);
-        const secondActionRow = new MessageActionRow().addComponents(channelIdInput);
-        const thirdActionRow = new MessageActionRow().addComponents(messageTextInput);
-        const lastActionRow = new MessageActionRow().addComponents(periodInput);
-        modal.addComponents(nameActionRow, firstActionRow, secondActionRow, thirdActionRow, lastActionRow);
-        await interaction.showModal(modal);
+    switch(interaction.customId) {
+        case 'viewStreams':
+            await onViewStreamsClick(interaction);
+            break;
+        case 'deleteStream': {
+            await onDeleteStreamClick(interaction);
+            break;
+        }
+        case 'viewAllInfo': {
+            await onViewStreamInfoClick(interaction);
+            break;
+        }
+        case 'show': {
+            await onCreationModalClick(interaction);
+            break;
+        }
     }
 })
 
-client.on('interactionCreate', interaction => {
-    if (!interaction.isModalSubmit()) return;
+const handleStartCommand = async (message) => {
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('show')
+                .setLabel('Добавить поток')
+                .setStyle('PRIMARY'),
+            new MessageButton()
+                .setCustomId('viewStreams')
+                .setLabel('Просмотреть потоки')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('viewAllInfo')
+                .setLabel('Полная инфа о потоках (чтоб токен заново не искать)')
+                .setStyle('SECONDARY'),
+            new MessageButton()
+                .setCustomId('deleteStream')
+                .setLabel('Удалить поток')
+                .setStyle('DANGER'),
+        );
+
+    await message.reply({ content: 'Выберите действие:', components: [row] });
+}
+
+const handleDeleteCommand = async (message) => {
+    const arg = message.content.split(' ')[1];
+    if (arg) {
+        let streamNumber = parseInt(arg);
+        if (streamNumber > 0 && streamNumber <= streams.length) {
+            clearInterval(streams[streamNumber - 1].intervalId);
+            const stream = streams.at(streamNumber);
+            await deleteStream(stream);
+            streams.splice(streamNumber - 1, 1);
+            message.reply(`Поток ${streamNumber} успешно удален`);
+        } else {
+            message.reply('Неверный номер потока');
+        }
+    } else {
+        message.reply('Неверный формат данных. Пожалуйста, введите данные в следующем формате:\n/deletestream Номер потока');
+    }
+}
+
+const onFormSubmit = (interaction) => {
     const name = interaction.fields.getTextInputValue('nameInput');
     const token = interaction.fields.getTextInputValue('tokenInput');
     const channelId = interaction.fields.getTextInputValue('channelIdInput');
@@ -144,83 +226,29 @@ client.on('interactionCreate', interaction => {
         const payload = { content: `${stream.messageText}` }
         axios.post(URL, payload, { headers: { 'authorization': stream.token } })
             .then(async (response) => {
-                interaction.reply({content: 'Поток успешно создан'});
                 stream.intervalId = startPosting(stream);
                 await saveStreamToDb(stream);
                 streams.push(stream);
-
+                await interaction.reply({content: 'Поток успешно создан'});
             })
-            .catch((error) => {
-                interaction.reply({content: 'Ошибка создания, повторите попытку!'});
+            .catch(async (error) => {
+                console.log(error);
+                await interaction.reply({content: 'Ошибка создания, повторите попытку!'});
             });
     }
-});
+}
 
-client.on('messageCreate', async (message) => {
-    console.log(message);
-    if (message.content === '/start') {
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('show')
-                    .setLabel('Добавить поток')
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId('viewStreams')
-                    .setLabel('Просмотреть потоки')
-                    .setStyle('SECONDARY'),
-                new MessageButton()
-                    .setCustomId('viewAllInfo')
-                    .setLabel('Полная инфа о потоках (чтоб токен заново не искать)')
-                    .setStyle('SECONDARY'),
-                new MessageButton()
-                    .setCustomId('deleteStream')
-                    .setLabel('Удалить поток')
-                    .setStyle('DANGER'),
-            );
-
-        await message.reply({ content: 'Выберите действие:', components: [row] });
-    }
-});
-
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isButton()) return;
-    if (interaction.customId === 'viewStreams') {
-        let streamsText = streams.map((stream, index) => `Поток ${index + 1}: ${stream.name}`).join('\n');
-        await interaction.reply({ content: streamsText || 'Нет активных потоков' });
-    } else if (interaction.customId === 'deleteStream') {
-        const embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Введите номер потока')
-            .setDescription('Введите номер потока, который вы хотите удалить, в следующем формате:\n/deletestream Номер потока');
-        await interaction.reply({ embeds: [embed] });
-    } else if (interaction.customId === 'viewAllInfo') {
-        const description = streams.map((el, index )=> `${index + 1}: Название - ${el.name}, Токен - ${el.token}, Айди канала - ${el.channelId}\n`)
-        const embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Все потоки')
-            .setDescription(description.join(""));
-        await interaction.reply({ embeds: [embed] });
-    }
+client.on('interactionCreate', interaction => {
+    if (!interaction.isModalSubmit()) return;
+    onFormSubmit(interaction);
 });
 
 client.on('messageCreate', async (message) => {
     if (message.content.startsWith('/deletestream')) {
-        const arg = message.content.split(' ')[1];
-        if (arg) {
-            let streamNumber = parseInt(arg);
-            if (streamNumber > 0 && streamNumber <= streams.length) {
-                clearInterval(streams[streamNumber - 1].intervalId);
-                const stream = streams.at(streamNumber);
-                await deleteStream(stream);
-                streams.splice(streamNumber - 1, 1);
-                message.reply(`Поток ${streamNumber} успешно удален`);
-            } else {
-                message.reply('Неверный номер потока');
-            }
-        } else {
-            message.reply('Неверный формат данных. Пожалуйста, введите данные в следующем формате:\n/deletestream Номер потока');
-        }
+        await handleDeleteCommand(message);
+    }
+    if (message.content === '/start') {
+        await handleStartCommand(message);
     }
 });
 
@@ -233,7 +261,7 @@ function startPosting(stream) {
                 console.log(`Сообщение отправлено, ${new Date()}`)
             })
             .catch((error) => {
-                console.log(`Сообщение не отправлено, ${new Date()}`)
+                console.log(`Сообщение не отправлено, ${new Date()}`, error)
             });
     }, stream.intervalInMinutes * 60 * 1000);
 }
@@ -243,8 +271,9 @@ app.get('/api', (req, res) => {
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
 
-const initApp = () => {
-    loadStreamsFromDb();
+const initApp = async () => {
+    streams = [];
+    await loadStreamsFromDb();
     streams.forEach(stream => {
         startPosting(stream);
     });
